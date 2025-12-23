85e3aba6c5080c6ff7f722a0faffc8f89fb4ef23
Nithesh Balaji <balaji22fb@gmail.com>
2025-12-23 23:54:47 +0530

    Initial commit

--- scripts/app.js
+++ scripts/app.js
@@ -1,2 +1,9 @@
 console.log("E-Commerce Website Loaded");
 
+// Hamburger toggle
+const hamburger = document.querySelector('.hamburger');
+const navMenu = document.querySelector('.nav-menu');
+
+hamburger.addEventListener('click', () => {
+    navMenu.classList.toggle('active');
+});
